7dcf449a124d63ff0443a208c80d7b44200855fa
leonardosparavalos-creator <leonardosparavalos@gmail.com>
2025-08-31 12:55:06 +0300

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { chromium } = require("playwright-chromium");
+const { chromium } = require("playwright");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
